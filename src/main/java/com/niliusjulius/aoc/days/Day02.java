5f180adeec3a9b6fac310c0cd4316516178b79df
nhouten <nielsvhouten@gmail.com>
2024-12-03 07:27:32 +0100

    Improved day 2

--- src/main/java/com/niliusjulius/aoc/days/Day02.java
+++ src/main/java/com/niliusjulius/aoc/days/Day02.java
@@ -16,7 +16,7 @@ public class Day02 {
     private static int part1(List<String> input) {
         int safe = 0;
         for (String report : input) {
-            String[] points = report.split(" ");
+            List<Integer> points = Arrays.stream(report.split(" ")).map(Integer::parseInt).toList();
             if (isSafe(points)) {
                 safe++;
             }
@@ -24,78 +24,64 @@ public class Day02 {
         return safe;
     }
 
-    private static boolean isSafe(String[] points) {
-        boolean increasing = Integer.parseInt(points[1]) >= Integer.parseInt(points[0]);
-        for (int i = 1; i < points.length; i++) {
-            if (Integer.parseInt(points[i]) > Integer.parseInt(points[i-1]) && increasing) {
-                if (Math.abs(Integer.parseInt(points[i]) - Integer.parseInt(points[i-1])) > 3) {
-                    return false;
-                }
-            } else if (Integer.parseInt(points[i]) < Integer.parseInt(points[i-1]) && !increasing) {
-                if (Math.abs(Integer.parseInt(points[i]) - Integer.parseInt(points[i-1])) > 3) {
-                    return false;
-                }
-            } else {
+    private static boolean isSafe(List<Integer> points) {
+        boolean increasing = points.get(1) >= points.get(0);
+        for (int i = 1; i < points.size(); i++) {
+            if (differenceTooLarge(points.get(i), points.get(i-1))) {
+                return false;
+            }
+            if (increasing && points.get(i) <= points.get(i-1)) {
+                return false;
+            } else if (!increasing && points.get(i) >= points.get(i-1)) {
                 return false;
             }
         }
         return true;
     }
 
+    private static boolean differenceTooLarge(int a, int b) {
+        return Math.abs(a-b) > 3;
+    }
+
     private static int part2(List<String> input) {
         int safe = 0;
-        int index = 0;
         for (String report : input) {
-            String[] points = report.split(" ");
-            List<String> pointsList = new LinkedList<>(Arrays.asList(points));
-            List<String> pointsList2 = new LinkedList<>(Arrays.asList(points));
+            List<Integer> points = Arrays.stream(report.split(" ")).map(Integer::parseInt).toList();
+            List<Integer> pointsList = new LinkedList<>(points);
+            List<Integer> pointsList2 = new LinkedList<>(points);
             pointsList2.removeFirst();
             if (isSafe2(pointsList, false) || isSafe2(pointsList2, true)) {
                 safe++;
-//                System.out.println(index);
             }
-            index++;
         }
         return safe;
     }
 
-    private static boolean isSafe2(List<String> points, boolean removed) {
-        boolean increasing = Integer.parseInt(points.get(1)) >= Integer.parseInt(points.get(0));
+    private static boolean checkSubsets(List<Integer> points, boolean removed, int i) {
+            if (!removed) {
+                List<Integer> copy1 = new LinkedList<>(points);
+                List<Integer> copy2 = new LinkedList<>(points);
+                copy1.remove(i);
+                copy2.remove(i-1);
+                return isSafe2(copy1, true) || isSafe2(copy2, true);
+            } else {
+                return false;
+            }
+    }
+
+    private static boolean isSafe2(List<Integer> points, boolean removed) {
+        boolean increasing = points.get(1) >= points.get(0);
         for (int i = 1; i < points.size(); i++) {
-            if (Integer.parseInt(points.get(i)) > Integer.parseInt(points.get(i-1)) && increasing) {
-                if (Math.abs(Integer.parseInt(points.get(i)) - Integer.parseInt(points.get(i-1))) > 3) {
-                    if (!removed) {
-                        List<String> copy1 = new LinkedList<>(points);
-                        List<String> copy2 = new LinkedList<>(points);
-                        copy1.remove(i);
-                        copy2.remove(i-1);
-                        return isSafe2(copy1, true) || isSafe2(copy2, true);
-                    } else {
-                        return false;
-                    }
+            if (increasing && points.get(i) > points.get(i-1)) {
+                if (differenceTooLarge(points.get(i), points.get(i-1))) {
+                    return checkSubsets(points, removed, i);
                 }
-            } else if (Integer.parseInt(points.get(i)) < Integer.parseInt(points.get(i-1)) && !increasing) {
-                if (Math.abs(Integer.parseInt(points.get(i)) - Integer.parseInt(points.get(i-1))) > 3) {
-                    if (!removed) {
-                        List<String> copy1 = new LinkedList<>(points);
-                        List<String> copy2 = new LinkedList<>(points);
-                        copy1.remove(i);
-                        copy2.remove(i-1);
-                        return isSafe2(copy1, true) || isSafe2(copy2, true);
-                    } else {
-                        return false;
-                    }
+            } else if (!increasing && points.get(i) < points.get(i-1)) {
+                if (differenceTooLarge(points.get(i), points.get(i-1))) {
+                    return checkSubsets(points, removed, i);
                 }
             } else {
-                if (!removed) {
-                    List<String> copy1 = new LinkedList<>(points);
-                    List<String> copy2 = new LinkedList<>(points);
-                    copy1.remove(i);
-                    copy2.remove(i-1);
-                    return isSafe2(copy1, true) || isSafe2(copy2, true);
-                } else {
-                    return false;
-                }
+                return checkSubsets(points, removed, i);
             }
         }
         return true;
