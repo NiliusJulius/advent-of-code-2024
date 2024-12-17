7c5ab495d1edb09437830675b05ada4f70badf33
Niels van Houten <niels@ubeeo.nl>
2024-12-17 17:20:42 +0100

    Add day 17 pt2

--- src/main/java/com/niliusjulius/aoc/days/Day17.java
+++ src/main/java/com/niliusjulius/aoc/days/Day17.java
@@ -11,30 +11,58 @@ public class Day17 {
 
     public static void main(String[] args) {
         List<String> input = Reader.readLinesAsList("day17");
-        int a = Integer.parseInt(input.get(0).split(" ")[2]);
-        int b = Integer.parseInt(input.get(1).split(" ")[2]);
-        int c = Integer.parseInt(input.get(2).split(" ")[2]);
+        long a = Long.parseLong(input.get(0).split(" ")[2]);
+        long b = Long.parseLong(input.get(1).split(" ")[2]);
+        long c = Long.parseLong(input.get(2).split(" ")[2]);
         String programLine = input.get(4).split(" ")[1];
         List<Integer> program = Arrays.stream(programLine.split(",")).map(Integer::parseInt).toList();
 
         System.out.println(part1(program, a, b, c));
+        System.out.println(part2(program));
     }
 
-    private static String part1(List<Integer> program, int a, int b, int c) {
+    private static String part1(List<Integer> program, long a, long b, long c) {
+        List<Integer> output = determineOutput(program, a, b, c);
+        return output.stream().map(String::valueOf).collect(Collectors.joining(","));
+    }
+
+    private static String part2(List<Integer> program) {
+        long a = 0;
+        long b = 0;
+        long c = 0;
+        List<Long> workingValues = new ArrayList<>();
+        recurse(program, a, b, c, workingValues, program.size() - 1);
+        return workingValues.stream().min(Long::compareTo).orElse(0L).toString();
+    }
+
+    private static void recurse(List<Integer> program, long a, long b, long c, List<Long> workingValues, int programIndex) {
+        for (int j = 0; j < 8; j++) {
+            List<Integer> output = determineOutput(program, a+j, b, c);
+            if (output.getFirst().equals(program.get(programIndex))) {
+                if (output.size() == program.size()) {
+                    workingValues.add(a+j);
+                } else {
+                    recurse(program, (a+j) * 8, b, c, workingValues, programIndex - 1);
+                }
+            }
+        }
+    }
+
+    private static List<Integer> determineOutput(List<Integer> program, long a, long b, long c) {
         int programLength = program.size();
         int programIndex = 0;
         List<Integer> output = new ArrayList<>();
         while (programIndex < programLength) {
             int opCode = program.get(programIndex);
             if (opCode == 0) {
-                int combo = getComboValue(program.get(programIndex + 1), a, b, c);
-                a = (int) (a / Math.pow(2, combo));
+                long combo = getComboValue(program.get(programIndex + 1), a, b, c);
+                a = (long) (a / Math.pow(2, combo));
                 programIndex += 2;
             } else if (opCode == 1) {
                 b = b ^ program.get(programIndex + 1);
                 programIndex += 2;
             } else if (opCode == 2) {
-                int combo = getComboValue(program.get(programIndex + 1), a, b, c);
+                long combo = getComboValue(program.get(programIndex + 1), a, b, c);
                 b = combo % 8;
                 programIndex += 2;
             } else if (opCode == 3) {
@@ -47,24 +75,23 @@ public class Day17 {
                 b = b ^ c;
                 programIndex += 2;
             } else if (opCode == 5) {
-                int combo = getComboValue(program.get(programIndex + 1), a, b, c);
-                output.add(combo % 8);
+                long combo = getComboValue(program.get(programIndex + 1), a, b, c);
+                output.add((int) (combo % 8));
                 programIndex += 2;
             } else if (opCode == 6) {
-                int combo = getComboValue(program.get(programIndex + 1), a, b, c);
-                b = (int) (a / Math.pow(2, combo));
+                long combo = getComboValue(program.get(programIndex + 1), a, b, c);
+                b = (long) (a / Math.pow(2, combo));
                 programIndex += 2;
             } else if (opCode == 7) {
-                int combo = getComboValue(program.get(programIndex + 1), a, b, c);
-                c = (int) (a / Math.pow(2, combo));
+                long combo = getComboValue(program.get(programIndex + 1), a, b, c);
+                c = (long) (a / Math.pow(2, combo));
                 programIndex += 2;
             }
         }
-
-        return output.stream().map(String::valueOf).collect(Collectors.joining(","));
+        return output;
     }
 
-    private static int getComboValue(int value, int a, int b, int c) {
+    private static long getComboValue(int value, long a, long b, long c) {
         return switch (value) {
             case 0,1,2,3 -> value;
             case 4 -> a;
